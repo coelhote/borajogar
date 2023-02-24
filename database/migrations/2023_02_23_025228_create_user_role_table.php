c06fdd3497863ea9166596e198d3c3706d173363
coelhote <leonardocoelho@ymail.com>
2023-02-23 21:52:23 -0300

    Initial commit

---
+++ database/migrations/2023_02_23_025228_create_user_role_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_role', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        DB::table('user_role')->insert(
+            array(
+                'name' => 'admin'
+            ),
+            array(
+                'name' => 'establishment'
+            ),
+            array(
+                'name' => 'team'
+            ),
+            array(
+                'name' => 'player'
+            ),
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_role');
+    }
+};
